6f3ad9801371766838fe3798962b270264dcccce
Legopitstop <1589lego@gmail.com>
2021-09-01 12:54:24 -0500

    removed functions that are in script.js

--- widget.js
+++ widget.js
@@ -1,63 +1,12 @@
 // https://legopitstop.github.io/widgets/widget.html?url=https://github.com/legopitstop/Lots-More-Food-Mods/issues&theme=dark
 
-const WEBSITE = 'https://legopitstop.github.io/widgets/widget.html'
-const JSONURL = parseURLParams(location.search)
-const URL = getJSONURL()
+var WEBSITE = 'https://legopitstop.github.io/widgets/widget.html'
+var JSONURL = parseURLParams(location.search)
+var JSON = getJSONURL()
 
 $(document).ready(function () {
-    adjustPreviewSize();
-    widget(URL);
+    widget(JSON);
 });
-
-
-
-function preserveRatio(input) {
-    var width = document.getElementById('width')
-    var height = document.getElementById('height')
-    var ratio = document.getElementById('ratio')
-    adjustPreviewSize()
-    if (checked('useratio')) {
-        if (input == 'width') {
-            height.value = Math.floor(width.value * ratio.value);
-        } else if (input == 'height') {
-            width.value = Math.floor(height.value / ratio.value);
-        } else if (input == 'other') {
-            height.value = Math.floor(width.value * ratio.value);
-        } else {
-            console.error('unknown ratio input');
-        }
-    }
-}
-
-function adjustPreviewSize() {
-    const frameWidth = document.querySelector(".preview-frame").clientWidth;
-    const previewWidth = document.querySelector(".preview").clientWidth;
-    var center = (previewWidth - frameWidth) / 2
-    $('.preview').css({
-        'padding-left': center
-    });
-}
-function updatePreview() {
-    var defaultRepo = 'https://github.com/legopitstop/datapacks/issues'
-    var type = document.getElementById('type');
-    var labels = checked('labels')
-    var theme = document.getElementById('theme');
-    var url = document.getElementById('repo-url');
-    var width = document.getElementById('width');
-
-
-    if (url.value <= 0) {
-        document.getElementById('repo-url').value = defaultRepo;
-        url = defaultRepo;
-    } else { var url = url.value; }
-    var src = WEBSITE + '?type=' + type.value + '&url=' + encodeURIComponent(url) + '&theme=' + theme.value + '&labels=' + labels;
-    document.querySelector('.preview-frame').src = src;
-    document.querySelector('.preview-frame').width = width.value;
-    document.querySelector('.preview-frame').height = height.value;
-    document.getElementById('frame-output').innerHTML = '<iframe src="' + WEBSITE + '?type=' + type.value + '&url=' + encodeURIComponent(url) + '&theme=' + theme.value + '&labels=' + labels + '" width="' + width.value + '" height="' + height.value + '" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>';
-    adjustPreviewSize();
-}
-
 function getJSONURL() {
     if (JSONURL && JSONURL.url[0]) {
         return JSONURL.url[0];
@@ -65,7 +14,6 @@ function getJSONURL() {
         return 'https://github.com/legopitstop/datapacks/issues';
     }
 }
-
 function widget(url) {
     if (JSONURL) {
         if (JSONURL.type) {
@@ -85,7 +33,6 @@ function widget(url) {
         console.error('oops! something went wrong.')
     }
 }
-
 function githubWidget(repo) {
     if (repo != '') {
         const HEADER = $('.widget_header')
@@ -150,7 +97,6 @@ function githubWidget(repo) {
         console.log('missing url!')
     }
 }
-
 function customWidget(url) {
     if (url != '') {
         console.log('Note that custom widgets are still in early development.')
